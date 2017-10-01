0316fc50ec83915cff1ba48f31626df3700af179
Alesya Polovets <alesya.polovets@gmail.com>
2017-10-01 23:57:29 +0300

    Implemented mult

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var number1 = parseInt(first);
+    var number2 = parseInt(second);
+    return String(number1*number2);
 }
